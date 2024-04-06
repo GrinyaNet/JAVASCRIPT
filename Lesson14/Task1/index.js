ceb91008dff3d6a82572796650f402e13e3ac684
GrinyaNet <igrinya@yahoo.com>
2024-04-06 11:19:24 +0300

    Task1

---
+++ Lesson14/Task1/index.js
@@ -0,0 +1,13 @@
+let message = 'Just learn it';
+
+ function sendMessage(name) {
+    const sender ='Gromcode';
+    console.log(`${name}, ${message}! You ${sender}`);
+}
+
+ function setMessage(text) {
+    message = text;
+}
+
+setMessage('Good job');
+sendMessage('Ann');
