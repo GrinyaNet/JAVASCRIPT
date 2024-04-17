d0e513365debc8e7eb9f12449e5aaf69f8e1fd65
GrinyaNet <igrinya@yahoo.com>
2024-04-17 11:54:24 +0300

    Task3

--- Lesson19/Task3/index.js
+++ Lesson19/Task3/index.js
@@ -1,16 +1,31 @@
 function User(name, age) {
     this.name = name;
     this.age = age;
-
-    this.sayHi() {
-        console.log(`Hi, I am ${this.name}`);
-    },
-
-    this.requestNewPhoto() {
-        console.log(`New photo request was sent for ${this.name}`);
-    },
-
-    this.setAge() {
-        console.log(`New photo request was sent for ${this.name}`);
-    },
 }
+User.prototype.sayHi = function () {
+    console.log(`Hi, I am ${this.name}`);
+}
+
+User.prototype.requestNewPhoto = function () {
+    console.log(`New photo request was sent for ${this.name}`);
+}
+
+User.prototype.setAge = function (value) {
+
+    if (value < 0) {
+        return false;
+    }
+    this.age = value;
+    if (value >= 25) {
+        this.requestNewPhoto();
+    }
+return value;
+
+
+};
+//};
+
+const user1 = new User('Bob', 25);
+
+//console.log(user1);
+user1.setAge(25);
