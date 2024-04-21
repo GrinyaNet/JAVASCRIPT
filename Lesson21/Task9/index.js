9aaf6ce76fa4aa1ca5489e06a6e31fd9cda4eec7
GrinyaNet <igrinya@yahoo.com>
2024-04-21 17:00:20 +0300

    Task9

---
+++ Lesson21/Task9/index.js
@@ -0,0 +1,11 @@
+function finishList()  {
+    let elem = document.querySelector('.list');
+    let gg = index(elem);
+    //let gg = elem.textContent;
+    //let dom = document.createElement('button');
+    //dom.textContent = buttonText;
+    //elem.append(dom);
+    console.dir(gg);
+}
+
+finishList();
