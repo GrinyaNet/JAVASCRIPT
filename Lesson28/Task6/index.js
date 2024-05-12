f0b64b72c662a3cc5d60e3fff82b9b8439a544e5
GrinyaNet <igrinya@yahoo.com>
2024-05-12 17:54:30 +0300

    Task6

--- Lesson28/Task6/index.js
+++ Lesson28/Task6/index.js
@@ -1,34 +1,3 @@
-let memo = {};
-
-const maxFibonacci1 = n => {
-    if (n in memo)
-        return memo[n];
-    if (n <= 1)
-        return n;
-    memo[n] = maxFibonacci1(n - 1) + maxFibonacci1(n - 2);
-    return memo[n];
-}
-
-console.log(maxFibonacci1(10)); // Вывод: 55
-console.log(memo);
-
-// function fibonacciLoop(n) {
-//     if (n <= 1)
-//         return n;
-
-//     let a = 0, b = 1;
-//     let temp;
-//     for (let i = 2; i <= n; i++) {
-//         temp = b;
-//         b = a + b;
-//         a = temp;
-
-//     }
-//     return b;
-// }
-
-// console.log(fibonacciLoop(10));
-
 
 const maxFibonacci = n => {
     const arr = [];
