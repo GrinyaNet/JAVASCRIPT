3aa5c83558a7e0aea67af86c1e2960e2f00c6474
GrinyaNet <igrinya@yahoo.com>
2024-05-17 13:32:44 +0300

    Task5

---
+++ Lesson30/Task5/index.js
@@ -0,0 +1,31 @@
+export const addImageV2 = url => {
+    // put your code here
+    return new Promise((resolve, reject) => {
+        
+            const img = document.createElement('img');
+            img.setAttribute('alt', 'User avatar');
+            img.src = url;
+          
+            const pageElem = document.querySelector('.page');
+            pageElem.append(img);
+          
+            const onImageLoaded = () => {
+              const { width, height } = img;
+              resolve({ width, height });
+            };
+          
+            const onImageLoadError = () => reject(new Error('Image load failed'));
+          
+            img.addEventListener('load', onImageLoaded);
+          
+            img.addEventListener('error', onImageLoadError);
+          
+    });
+  };
+  
+  // examples
+  
+  addImageV2('https://server.com/image.png')
+    .then(data => console.log(data)) // ==> { width: 200, height: 100 }
+    .catch(error => console.log(error)); // ==> 'Image load failed'
+  
