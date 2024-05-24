d2481356525468ac2bc82be0403ab97aa58601cb
GrinyaNet <igrinya@yahoo.com>
2024-05-24 07:43:07 +0300

    24

--- Lesson33/Task2/index.js
+++ Lesson33/Task2/index.js
@@ -1,10 +1,33 @@
-//const comint = 'api.github.com/USERID/REPOID/commits?per_page=100';
-const comint = 'https://api.github.com/USERID/REPOID/commits?per_page=100';
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-const getMostActiveDevs = () => {
-    return fetch(comint)
-        .then(response => console.log(response.json()));
-        //.then(response => response.json());
+function getTasksList() {
+  // put your code here
+  return fetch(baseUrl)        
+        .then(response => response.json());
 }
 
-getMostActiveDevs({ days: 7, userId: 'userId', repoId: 'repoId'});
+function getTaskById(taskId) {
+  // put your code here
+  return fetch(`${baseUrl}?id=${taskId}`)
+  .then(response => response.json())  
+}
+
+// examples
+getTasksList().then(tasksList => {
+  console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
+});
+
+getTaskById('2').then(taskData => {
+  console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
+});
+
+// let cities = [{ id: 121, name: 'г. Урюпинск' }, { id: 122, name: 'г. Париж' }, { id: 123, name: 'г. Москва' }, { id: 124, name: 'г. Штормград' }];
+// let searchTerm = 'г. Москва';
+// let cityId = cities.find(city => city.name === searchTerm).id
+// console.log(cityId);
+
+// let response = fetch(protectedUrl, {
+//     headers: {
+//       Authentication: 'secret'
+//     }
+//   });
