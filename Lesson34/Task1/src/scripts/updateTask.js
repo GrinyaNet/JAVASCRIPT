53328feac0562095012e2dba7f424a79830eb699
GrinyaNet <igrinya@yahoo.com>
2024-05-25 12:55:57 +0300

    Task1

--- Lesson34/Task1/src/scripts/updateTask.js
+++ Lesson34/Task1/src/scripts/updateTask.js
@@ -2,7 +2,8 @@ import { renderTasks } from './renderer.js';
 import { getItem, setItem } from './storage.js';
 
 export const onToggleTask = e => {
-    const isCheckbox = e.target.classList.contains('list-item__checkbox');
+    //const isCheckbox = e.target.classList.contains('list-item__checkbox');
+    const isCheckbox = e.target.classList.contains('list__item-checkbox');
 
     if (!isCheckbox) {
         return;
