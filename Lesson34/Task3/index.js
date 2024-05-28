92374f68b1a61378ad33e98b49d390062254957e
GrinyaNet <igrinya@yahoo.com>
2024-05-28 11:41:25 +0300

    Task3

--- Lesson34/Task3/index.js
+++ Lesson34/Task3/index.js
@@ -1,24 +1,5 @@
-//const baseUrl = 'https://6651f41920f4f4c44279464d.mockapi.io/api/v1/users';
+const baseUrl = 'https://6651f41920f4f4c44279464d.mockapi.io/api/v1/users';
 
-const loginUser = () => {
-    const emailUser = document.querySelector('input[type="email"]');
-    const nameUser = document.querySelector('input[type="text"]');
-    const passwordUser = document.querySelector('input[type="password"]');
-    
-    
-    const email = emailUser.value;
-    const name = nameUser.value;
-    const password = passwordUser.value;
-    
-    const user = {
-        email,
-        name,
-        password,
-    }
-    console.log(user);
-
-    alert('ytllo');
-}
 
 const submitBtn = document.querySelector('.submit-button');
 const inputs = document.querySelectorAll('.form-input');
@@ -26,37 +7,39 @@ const inputs = document.querySelectorAll('.form-input');
 
 for (let input of inputs) {
     input.addEventListener('blur', () => {
-        if(input.reportValidity()) {
-        console.log("OK!!!");
-        submitBtn.removeAttribute('disabled');
-        //submitBtn.setAttribute('disabled', false);
+                   if (input.reportValidity()) {
+            console.log("OK!!!"); 
+            submitBtn.removeAttribute('disabled');
         } else {
-            console.log("BED!!!");
+            
+            return buttonElem.setAttribute('disabled');
         }
+       
     });
-    //buttonElem.setAttribute('disable', boolean);
+    
 }
 
-// function validate(){
-//     if(!registerForm.username.checkValidity()){
+const loginUser = (event) => {
+    event.preventDefault();
+    const myFormData = new FormData(form);
+    
+    const formDataObj = Object.fromEntries(myFormData);
+    console.log(formDataObj);
 
-//         console.log("Username is not valid");
-//     }
-//     if(!registerForm.age.checkValidity()){
-//         console.log("Age is not valid");
-//     }
-//     if(!registerForm.checkValidity()){
-//         console.log("Form data is not valid");
-//     }
-// }
+    fetch(baseUrl, {
+        method: 'POST',
+         headers: {
+             'Content-Type': 'application/json;charset=utf-8'
+         },       
+        body: JSON.stringify(formDataObj)
+    })
+        .then(response => response.json())        
+        .then((data) => alert(JSON.stringify(data)))
+    form.reset();
+}
 
 
-// function User(emailUser, nameUser, passwordUser) {
-//     this.emailUser = emailUser;
-//     this.nameUser = nameUser;
-//     this.passwordUser = passwordUser;
-// }
+const form = document.querySelector('.login-form');
 
-
-//const submitBtn = document.querySelector('.submit-button');
 submitBtn.addEventListener('click', loginUser);
+
