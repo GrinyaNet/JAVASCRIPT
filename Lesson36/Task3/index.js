2bccc9fffa2a4cde0a96adb420d959cbc0f2db29
GrinyaNet <igrinya@yahoo.com>
2024-05-31 10:09:04 +0300

    Task3

--- Lesson36/Task3/index.js
+++ Lesson36/Task3/index.js
@@ -5,9 +5,25 @@
  * @return {promise}
  */
 
-const getUsersBlogs = users => {
-  // put your code here
+const getUsersBlogs = async users => {
+  
+  try {
+    const arrayFetchUsers = users.map(user => fetch(`https://api.github.com/users/${user}`).then((response) => response.json()))
+    const promise = Promise.all(arrayFetchUsers);
+
+
+if (promise.ok) {
+           return null;
+         }
+
+    const resalt = await promise;    
+    const resultArr = resalt.map((item) => (item.blog));    
+    return resultArr;
+  } catch (err) {
+      throw new Error('Failed to get user data');
+     } 
 };
+
 
 // examples
 getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList)); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
