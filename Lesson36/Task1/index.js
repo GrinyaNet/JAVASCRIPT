a23bb574885f85a3f05edd17c26c52914ff50b88
GrinyaNet <igrinya@yahoo.com>
2024-05-31 07:38:08 +0300

    Task1

--- Lesson36/Task1/index.js
+++ Lesson36/Task1/index.js
@@ -1,5 +1,19 @@
 'use strict';
 
-export const fetchUser = userId => {
+ const  fetchUser = async userId => {
   // put your code here
+try {
+  const responce = await fetch(`https://api.github.com/users/${userId}`)
+  if (!responce.ok) {
+    return null;
+  }
+  const userDate = await responce.json();
+  return userDate;
+} catch (err) {
+  throw new Error('Failed to fetch user');
+}
 };
+
+fetchUser('facebook')
+.then(userDate => console.log(userDate))
+.catch(err => alert(err.message));
